1be80eded8273e5ce0e2f431e6f348b1e0a86938
Matthew Bishop <bishop@atmos.colostate.edu>
2011-05-04 16:47:04 -0600

    Call funit module functions correctly in generated code

--- generate_code.c
+++ generate_code.c
@@ -20,17 +20,21 @@ void generate_test(struct TestRoutine *test, int *test_i)
         generate_test(test->next, test_i);
 
     *test_i += 1;
-    printf("  subroutine test%i\n", *test_i);
+    printf("  subroutine _funit_test%i(_funit_passed, _funit_message)\n", *test_i);
+    fputs("    implicit none\n", stdout);
+    fputs("    logical, intent(out) :: _funit_passed\n", stdout);
+    fputs("    character(*), intent(out) :: _funit_message\n\n", stdout);
+
     if (test->code)
         generate_code(test->code);
-    printf("  end subroutine test%i\n\n", *test_i);
+    printf("  end subroutine _funit_test%i\n\n", *test_i);
 }
 
 void generate_support(struct Code *code, const char *type)
 {
-    printf("  subroutine %s\n", type);
+    printf("  subroutine _funit_%s\n", type);
     generate_code(code);
-    printf("  end subroutine %s\n\n", type);
+    printf("  end subroutine _funit_%s\n\n", type);
 }
 
 void generate_test_call(struct TestSuite *suite,
@@ -43,10 +47,13 @@ void generate_test_call(struct TestSuite *suite,
 
     fputs("\n", stdout);
     if (suite->setup)
-        printf("  call setup\n");
-    printf("  call test%i\n", *test_i);
+        printf("  call _funit_setup\n");
+    printf("  call _funit_test%i(_funit_passed, _funit_message)\n", *test_i);
+    fputs("  call pass_fail(_funit_passed, _funit_message, \"", stdout);
+    fwrite(test->name, test->name_len, 1, stdout);
+    fputs("\")\n", stdout);
     if (suite->teardown)
-        fputs("  call teardown\n\n", stdout);
+        fputs("  call _funit_teardown\n\n", stdout);
 }
 
 void generate_suite(struct TestSuite *suite, int *suite_i)
@@ -57,8 +64,11 @@ void generate_suite(struct TestSuite *suite, int *suite_i)
         generate_suite(suite->next, suite_i);
 
     *suite_i += 1;
-    printf("subroutine suite%i\n", *suite_i);
+    printf("subroutine _funit_suite%i\n", *suite_i);
     fputs("  implicit none\n\n", stdout);
+    fputs("  use funit\n\n", stdout);
+    fputs("  character*512 :: _funit_message\n", stdout);
+    fputs("  integer :: _funit_passed\n\n", stdout);
     if (suite->code)
         generate_code(suite->code);
     if (suite->tests) {
@@ -74,19 +84,19 @@ void generate_suite(struct TestSuite *suite, int *suite_i)
         test_i = 0;
         generate_test(suite->tests, &test_i);
     }
-    printf("end subroutine suite%i\n", *suite_i);
+    printf("end subroutine _funit_suite%i\n", *suite_i);
 }
 
-void generate_suite_calls(struct TestSuite *suite, int *suite_i)
+void generate_suite_call(struct TestSuite *suite, int *suite_i)
 {
     if (suite->next)
-        generate_suite_calls(suite->next, suite_i);
+        generate_suite_call(suite->next, suite_i);
 
     *suite_i += 1;
     fputs("\n  call start_suite(\"", stdout);
     fwrite(suite->name, suite->name_len, 1, stdout);
     fputs("\")\n", stdout);
-    printf("  call suite%i\n", *suite_i);
+    printf("  call _funit_suite%i\n", *suite_i);
 }
 
 void generate_main(struct TestSuite *suite, int *suite_i)
@@ -94,7 +104,7 @@ void generate_main(struct TestSuite *suite, int *suite_i)
     fputs("\n\nprogram main\n", stdout);
     fputs("  use funit\n\n",  stdout);
     fputs("  call clear_stats\n", stdout);
-    generate_suite_calls(suite, suite_i);
+    generate_suite_call(suite, suite_i);
     fputs("\n  call report_stats\n", stdout);
     printf("end program main\n");
 }
